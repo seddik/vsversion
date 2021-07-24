22bd850dab59f5376270b4b924e164af7c2a1d85
LARABA MOHAMMED SEDDIK <mslaraba@gmail.com>
2021-07-24 19:42:09 +0100

    Initial working version

--- VSVersion/Program.cs
+++ VSVersion/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace VSVersion
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Count() != 1)
+            {
+                Console.WriteLine("!!! Error args count !!!");
+                return -1;
+            }
+            var items = args[0].Split("|", StringSplitOptions.RemoveEmptyEntries);
+            if (items.Count() != 2)
+            {
+                Console.WriteLine("!!! Bad formed argument !!!");
+                return -1;
+            }
+
+            var fullpath = Path.Combine(items[0], @"Properties\AssemblyInfo.cs");
+
+            if (!File.Exists(fullpath))
+            {
+                Console.WriteLine("!!! Not .NET project !!!");
+                return -1;
+            }
+            Console.WriteLine(fullpath);
+            var props = File.ReadAllText(fullpath).Split(@"AssemblyVersion(""");
+            if (props.Count() != 2)
+            {
+                Console.WriteLine("!!! Properties file invalid format !!!");
+                return -1;
+            }
+
+            var newtext = props[0];
+            newtext += @"AssemblyVersion(""" + DateTime.Now.ToString(items[1]) + @""")]";
+
+            newtext += string.Join(Environment.NewLine, props[1]
+    .Split(Environment.NewLine.ToCharArray())
+    .Skip(1)
+    .ToArray());
+
+            File.WriteAllText(fullpath, newtext);
+
+            Console.WriteLine();
+            return 0;
         }
     }
 }
